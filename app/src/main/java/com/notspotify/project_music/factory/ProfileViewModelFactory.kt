9f9d6a3fd4a21a37e9719e8697207f6fbd49ad28
Maxime <maximeld19@gmail.com>
2021-06-15 16:15:22 +0200

    song front

--- app/src/main/java/com/notspotify/project_music/factory/ProfileViewModelFactory.kt
+++ app/src/main/java/com/notspotify/project_music/factory/ProfileViewModelFactory.kt
@@ -1,13 +1,9 @@
 package com.notspotify.project_music.factory
 
-import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.notspotify.project_music.api.service.APIAccount
 import com.notspotify.project_music.api.service.APIArtist
 import com.notspotify.project_music.api.service.APISong
-import com.notspotify.project_music.ui.login.viewmodel.LoginViewModel
-import com.notspotify.project_music.ui.main.bibliotheque.viewmodel.BibliothequeViewModel
 import com.notspotify.project_music.ui.main.profile.viewmodel.ProfileViewModel
 
 @Suppress("UNCHECKED_CAST")
